fb75a39f533a6fc55d79c1a30ef9190b315c82cd
mwai-jnr <mwaivictor07@yahoo.com>
2023-03-22 06:13:13 -0700

    push_03

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lower case alphabet 10 times
+ *
+ * Return: 0 for success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char v;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (v = 'a' ; v <= 'z' ; v++)
+		_putchar('v');
+		_putchar('\n');
+	}
+	
+}
